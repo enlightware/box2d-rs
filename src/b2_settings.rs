f7076b6830f8dc426d1106dae5b550488134abab
Stéphane Magnenat <stephane@magnenat.net>
2026-06-12 13:34:16 +0200

    Set polygon vertex limit for Candli

--- src/b2_settings.rs
+++ src/b2_settings.rs
@@ -6,6 +6,6 @@
 /// For example for inches you could use 39.4.
 pub const B2_LENGTH_UNITS_PER_METER: f32 = 1.0;
 
-/// The maximum number of vertices on a convex polygon. You cannot increase
-/// this too much because b2BlockAllocator has a maximum object size.
-pub const B2_MAX_POLYGON_VERTICES: usize = 8;
+/// The maximum number of vertices on a convex polygon.
+/// Candli uses 12 to preserve compatibility with existing Planck-based games.
+pub const B2_MAX_POLYGON_VERTICES: usize = 12;
